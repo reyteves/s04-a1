817496ffb48614538a49b676e712982913aa812a
Rey Jesus Teves <jesus.reyteves@gmail.com>
2021-03-08 22:37:31 +0800

    update

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'
 
 export default function Jumbo() {
     return (
